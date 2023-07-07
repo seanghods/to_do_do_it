f3cdba5135044dae7df3ebe3bcb92362a069e0d4
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-07-07 09:53:35 -0700

    Add hover functions, Add CSS styling

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// let submit = document.getElementById("taskSubmit")
-// submit.addEventListener("submit",function() {
-//     let theText = document.createElement("p");
-//     let doneButton = document.createElement("button");
-//     let theBody = document.querySelector("body");
-//     theText.innerHTML = document.getElementsByClassName("inputBox");
-//     theBody.appendChild(theText);
-//     theBody.appendChild(doneButton);
-// })
-
-
 let submitButton = document.querySelector(".submit")
+submitButton.addEventListener("mouseover", highlightButton); 
+submitButton.addEventListener("mouseout", unHighlightButton);
 submitButton.addEventListener("click",createTask)
 
 function createTask(){
@@ -19,6 +10,8 @@ function createTask(){
     let doneButton = document.createElement("button");
     doneButton.classList.add("delbutton");
     doneButton.innerHTML = "Done"
+    doneButton.addEventListener("mouseover", crossText); 
+    doneButton.addEventListener("mouseout", unCrossText);
     doneButton.addEventListener("click", removeTask); 
     let theBody = document.querySelector("body");
     let input = document.getElementById('inputBox').value
@@ -29,6 +22,33 @@ function createTask(){
     theBody.appendChild(theTask);
 }
 
+function highlightButton(event) {
+    let taskElement = event.target;
+    taskElement.style.backgroundColor = "gold";
+}
+
+function unHighlightButton(event) {
+    let taskElement = event.target;
+    taskElement.style.backgroundColor = "darkblue";
+}
+
+function crossText(event) {
+    let taskElement = event.target.parentElement;
+    taskElement.style.textDecoration = "line-through";
+    taskElement.style.backgroundImage = "url(./images/newimg.png)";
+    taskElement.style.backgroundRepeat = "no-repeat";
+    taskElement.style.backgroundSize = "100% 285%";
+
+}
+
+function unCrossText(event) {
+    let taskElement = event.target.parentElement;
+    taskElement.style.textDecoration = "";
+    taskElement.style.removeProperty("background-image");
+    taskElement.style.removeProperty("background-repeat");
+    taskElement.style.removeProperty("background-size");
+
+}
 
 function removeTask(event) {
     let taskElement = event.target.parentElement;
